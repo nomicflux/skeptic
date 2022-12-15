596d04deff57737e8b939a78302f064b2b1a3d6b
Michael Anderson <manderson@drwholdings.com>
2022-12-15 12:52:20 -0600

    ma: update project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject skeptic "0.2.0"
-  :description "Testing out lein plugins"
+(defproject skeptic "0.3.0"
+  :description "Schema-based static type checking for Clojure"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT License"
+            :url "https://spdx.org/licenses/MIT.html"}
   :dependencies [[prismatic/schema                                 "1.1.12"]
                  [prismatic/plumbing                               "0.5.5"]]
-  :main skeptic.examples
+  :main skeptic.core
   :eval-in-leiningen true)
