7c0b16774e9f24be6da9f8e0d23a5f7464214bf4
Michael Anderson <manderson@drwholdings.com>
2023-01-06 16:08:24 -0600

    ma: analyse collections

--- skeptic/src/skeptic/analysis.clj
+++ skeptic/src/skeptic/analysis.clj
@@ -1,11 +1,39 @@
 (ns skeptic.analysis
   (:require [schema.core :as s]
             [plumbing.core :as p]
-            [clojure.walk :as walk]))
+            [clojure.walk :as walk])
+  (:import [schema.core Either]))
+
+(s/defn either?
+  [s]
+  (instance? Either s))
+
+(s/defn eitherize
+  [[t1 t2 & _r :as types]]
+  (cond
+    t2
+    (if (contains? types nil)
+      (s/maybe (eitherize (disj types nil)))
+      (apply s/either types))
+
+    t1 t1
+
+    :else s/Any))
 
 (defn annotate-expr
   [expr]
-  (walk/postwalk (let [n (atom 0)] (fn [f] {:expr f :idx (swap! n inc)}))
+  (walk/postwalk (let [n (atom 0)]
+                   (fn [f]
+                     (let [idx (swap! n inc)]
+                       (cond
+                        (instance? clojure.lang.IMapEntry f)
+                        {idx {:expr f :idx idx}}
+
+                        (map? f)
+                        {:expr (vals f) :idx idx :map? true}
+
+                        :else
+                        {:expr f :idx idx}))))
                  expr))
 
 (def resolve-local-vars
@@ -16,13 +44,46 @@
                      (:schema (get results idx))
                      v))))))
 
-(def resolve-output-schema
+(defn resolve-map-schema
+  [schema-fn]
+  (fn [results el]
+    (-> el
+        (update :schema
+             (fn [v]
+               (let [key-idxs (::key-placeholders v)
+                     val-idxs (::val-placeholders v)]
+                 (schema-fn (->> key-idxs (map (comp :schema (partial get results))))
+                            (->> val-idxs (map (comp :schema (partial get results))))) )))
+        (assoc :finished? true))))
+
+(defn resolve-coll-schema
+  [schema-fn]
+  (fn [results el]
+    (-> el
+        (update :schema
+                (fn [v]
+                  (if-let [idxs (::placeholders v)]
+                    (schema-fn (->> idxs (map (comp :schema (partial get results)))))
+                    v)))
+        (assoc :finished? true))))
+
+(def resolve-schema
   (fn [results el]
     (-> el
         (update :schema
              (fn [v]
                (if-let [idx (::placeholder v)]
                  (:schema (get results idx))
+                 v)))
+        (assoc :finished? true))))
+
+(def resolve-fn-output-schema
+  (fn [results el]
+    (-> el
+        (update :schema
+             (fn [v]
+               (if-let [idx (::placeholder v)]
+                 (:output (get results idx))
                  v)))
         (assoc :finished? true))))
 
@@ -48,8 +109,7 @@
                     [newvar varbody]]
                   (let [clause (assoc varbody
                                       :local-vars local-vars
-                                      :name (:expr newvar)
-                                      :dep-callback resolve-local-vars)]
+                                      :name (:expr newvar))]
                     {:local-vars (assoc local-vars
                                         (:expr newvar)
                                         {::placeholder (:idx varbody)})
@@ -61,12 +121,82 @@
         body-clauses
         (map (fn [clause]
                {:expr clause
-                :local-vars local-vars
-                :dep-callback resolve-local-vars})
+                :local-vars local-vars})
                 body)
 
         output-clause (last body-clauses)
         current-clause (assoc this
-                              :schema {::placeholder (:idx output-clause)}
-                              :dep-callback resolve-output-schema)]
+                              :schema {::placeholder (-> output-clause :expr :idx)}
+                              :dep-callback resolve-schema)]
     (concat let-clauses body-clauses [current-clause])))
+
+(defn analyse-application
+  [{:keys [expr local-vars]
+    :or {local-vars {}} :as this}]
+  (let [[f & args] expr
+
+        arg-clauses
+        (map (fn [clause]
+               (assoc clause
+                      :local-vars local-vars))
+                args)
+
+        fn-clause
+        (assoc f
+               :arity (count args)
+               :fn-position? true)]
+    (concat arg-clauses
+            [fn-clause
+             (assoc this
+             :schema {::placeholder (:idx fn-clause)}
+             :dep-callback resolve-fn-output-schema)])))
+
+(s/defn analyse-coll
+  [{:keys [expr map?] :as this}]
+  (cond
+    ;; As the postwalk rewrites things into maps, maps themselves need special annotation processing
+    map? (concat (mapcat :expr expr)
+                 [(assoc this
+                         :dep-callback (resolve-map-schema (fn [key-schemas val-schemas]
+                                                             {(eitherize key-schemas) (eitherize val-schemas)}))
+                         :schema {::key-placeholders (->> expr (map (comp :idx first :expr)))
+                                  ::val-placeholders (->> expr (map (comp :idx second :expr)))})])
+    (vector? expr) (concat expr
+                           [(assoc this
+                                   :dep-callback (resolve-coll-schema (fn [schemas] (vector (eitherize schemas))))
+                                   :schema {::placeholders (map :idx expr)})])
+    (set? expr) (concat expr
+                        [(assoc this
+                                :dep-callback (resolve-coll-schema (fn [schemas] (into #{} (eitherize schemas))))
+                                :schema {::placeholders (map :idx expr)})])
+    (or (list? expr) (seq? expr)) (concat expr
+                                          [(assoc this
+                                                  :dep-callback (resolve-coll-schema (fn [schemas] (vector (eitherize schemas))))
+                                                  :schema {::placeholders (map :idx expr)})])))
+
+(s/defn analyze-value :- {(s/optional-key :finished) [AnnotatedExpression]
+                          (s/optional-key :enqueue) [AnnotatedExpression]}
+  [{:keys [expr local-vars]
+    :or {local-vars {}} :as this} :- AnnotatedExpression]
+  (cond
+    (nil? expr) {:finished [(assoc this
+                                   :schema (s/maybe s/Any))]}
+    ;; (symbol? expr) (cond
+    ;;                  (contains? local-vars expr)
+    ;;                  (merge {:schema s/Any}
+    ;;                         (get local-vars expr))
+
+    ;;                  :else
+    ;;                  (get-from-dict dict arity expr {:schema s/Symbol}))
+    (var? expr) {:enqueue [(assoc this
+                                  :expr (or @expr (symbol expr)))]}
+    (int? expr) {:finished [(assoc this
+                         :schema s/Int)]}
+    (string? expr) {:finished [(assoc this
+                                      :schema s/Str)]}
+    (keyword? expr) {:finished [(assoc this
+                                       :schema s/Keyword)]}
+    (boolean? expr) {:finished [(assoc this
+                                       :schema s/Bool)]}
+    :else {:finished [(assoc this
+                             :schema (class expr))]}))
--- skeptic/test/skeptic/analysis_test.clj
+++ skeptic/test/skeptic/analysis_test.clj
@@ -37,7 +37,7 @@
                       schematize/macroexpand-all
                       sut/annotate-expr
                       sut/analyse-let)]
-    (is (= [{:expr 1, :idx 3, :local-vars {}}
+    (is (= [{:expr 1, :idx 3, :local-vars {}, :name 'x}
              {:expr
               {:expr [{:expr '+, :idx 5} {:expr 1, :idx 6} {:expr 'x, :idx 7}],
                :idx 8},
@@ -47,7 +47,8 @@
                {:expr [{:expr 'x, :idx 2} {:expr 1, :idx 3}], :idx 4}
                {:expr [{:expr '+, :idx 5} {:expr 1, :idx 6} {:expr 'x, :idx 7}],
                 :idx 8}],
-              :idx 9}]
+              :idx 9
+              :schema #:skeptic.analysis{:placeholder 8}}]
            (clean-callbacks analysed)))
     (is (= '(1 (+ 1 x) (let* [x 1] (+ 1 x)))
            (unannotate-expr analysed))))
@@ -57,40 +58,252 @@
                       sut/annotate-expr
                       sut/analyse-let)]
     (is (= [{:expr [{:expr '+, :idx 3} {:expr 1, :idx 4} {:expr 2, :idx 5}],
-              :idx 6,
-             :local-vars {}}
+             :idx 6,
+             :local-vars {},
+             :name 'x}
             {:expr [{:expr '+, :idx 8} {:expr 3, :idx 9} {:expr 'x, :idx 10}],
-              :idx 11,
-              :local-vars {'x #::sut{:placeholder 6}}}
-             {:expr
+             :idx 11,
+             :local-vars {'x #::sut{:placeholder 6}}
+             :name 'y}
+            {:expr
+             {:expr [{:expr '+, :idx 13} {:expr 7, :idx 14} {:expr 'x, :idx 15}],
+              :idx 16},
+             :local-vars {'x #::sut{:placeholder 6}
+                          'y #::sut{:placeholder 11}}}
+            {:expr
+             {:expr [{:expr '+, :idx 17} {:expr 'x, :idx 18} {:expr 'y, :idx 19}],
+              :idx 20},
+             :local-vars {'x #::sut{:placeholder 6}
+                          'y #::sut{:placeholder 11}}}
+            {:expr
+             [{:expr 'let*, :idx 1}
+              {:expr
+               [{:expr 'x, :idx 2}
+                {:expr [{:expr '+, :idx 3} {:expr 1, :idx 4} {:expr 2, :idx 5}],
+                 :idx 6}
+                {:expr 'y, :idx 7}
+                {:expr [{:expr '+, :idx 8} {:expr 3, :idx 9} {:expr 'x, :idx 10}],
+                 :idx 11}],
+               :idx 12}
               {:expr [{:expr '+, :idx 13} {:expr 7, :idx 14} {:expr 'x, :idx 15}],
-               :idx 16},
-              :local-vars {'x #::sut{:placeholder 6}
-                           'y #::sut{:placeholder 11}}}
-             {:expr
+               :idx 16}
               {:expr [{:expr '+, :idx 17} {:expr 'x, :idx 18} {:expr 'y, :idx 19}],
-               :idx 20},
-              :local-vars {'x #::sut{:placeholder 6}
-                           'y #::sut{:placeholder 11}}}
-             {:expr
-              [{:expr 'let*, :idx 1}
-               {:expr
-                [{:expr 'x, :idx 2}
-                 {:expr [{:expr '+, :idx 3} {:expr 1, :idx 4} {:expr 2, :idx 5}],
-                  :idx 6}
-                 {:expr 'y, :idx 7}
-                 {:expr [{:expr '+, :idx 8} {:expr 3, :idx 9} {:expr 'x, :idx 10}],
-                  :idx 11}],
-                :idx 12}
-               {:expr [{:expr '+, :idx 13} {:expr 7, :idx 14} {:expr 'x, :idx 15}],
-                :idx 16}
-               {:expr [{:expr '+, :idx 17} {:expr 'x, :idx 18} {:expr 'y, :idx 19}],
-                :idx 20}],
-              :idx 21}]
+               :idx 20}],
+             :idx 21
+             :schema #:skeptic.analysis{:placeholder 20}}]
            (clean-callbacks analysed)))
     (is (= '((+ 1 2)
              (+ 3 x)
              (+ 7 x)
              (+ x y)
              (let* [x (+ 1 2) y (+ 3 x)] (+ 7 x) (+ x y)))
+           (unannotate-expr analysed)))))
+
+(deftest analyse-application-test
+  (let [analysed (->> '(+ 1 x)
+                      schematize/macroexpand-all
+                      sut/annotate-expr
+                      sut/analyse-application)]
+    (is (= [{:expr 1, :idx 2, :local-vars {}}
+             {:expr 'x, :idx 3, :local-vars {}}
+             {:expr '+, :idx 1, :arity 2, :fn-position? true}
+            {:expr [{:expr '+, :idx 1} {:expr 1, :idx 2} {:expr 'x, :idx 3},]
+              :idx 4,
+              :schema #:skeptic.analysis{:placeholder 1}}]
+           (clean-callbacks analysed)))
+    (is (= '(1 x + (+ 1 x))
+           (unannotate-expr analysed))))
+  (let [analysed (->> '(f)
+                      schematize/macroexpand-all
+                      sut/annotate-expr
+                      sut/analyse-application)]
+    (is (= [{:expr 'f, :idx 1, :arity 0, :fn-position? true}
+            {:expr [{:expr 'f, :idx 1}],
+            :idx 2,
+            :schema #:skeptic.analysis{:placeholder 1}}]
+           (clean-callbacks analysed)))
+    (is (= '(f (f))
+           (unannotate-expr analysed))))
+  (let [analysed (->> '((f 1) 3 4)
+                      schematize/macroexpand-all
+                      sut/annotate-expr
+                      sut/analyse-application)]
+    (is (= [{:expr 3, :idx 4, :local-vars {}}
+             {:expr 4, :idx 5, :local-vars {}}
+            {:expr [{:expr 'f, :idx 1} {:expr 1, :idx 2}],
+              :idx 3,
+              :arity 2,
+              :fn-position? true}
+             {:expr
+              [{:expr [{:expr 'f, :idx 1} {:expr 1, :idx 2}], :idx 3}
+               {:expr 3, :idx 4}
+               {:expr 4, :idx 5}],
+              :idx 6,
+              :schema #:skeptic.analysis{:placeholder 3}}]
+           (clean-callbacks analysed)))
+    (is (= '(3 4 (f 1) ((f 1) 3 4))
+           (unannotate-expr analysed)))))
+
+(deftest analyse-coll-test
+  (let [analysed (->> '[1 2 :a "hello"]
+                      schematize/macroexpand-all
+                      sut/annotate-expr
+                      sut/analyse-coll)]
+    (is (= [{:expr 1, :idx 1}
+             {:expr 2, :idx 2}
+             {:expr :a, :idx 3}
+             {:expr "hello", :idx 4}
+             {:expr
+              [{:expr 1, :idx 1}
+               {:expr 2, :idx 2}
+               {:expr :a, :idx 3}
+               {:expr "hello", :idx 4}],
+              :idx 5,
+              :schema #:skeptic.analysis{:placeholders '(1 2 3 4)}}]
+           (clean-callbacks analysed)))
+    (is (= [1 2 :a "hello" '(1 2 :a "hello")]
+           (unannotate-expr analysed))))
+  (let [analysed (->> '#{1 2 :a "hello"}
+                      schematize/macroexpand-all
+                      sut/annotate-expr
+                      sut/analyse-coll)]
+    (is (= [{:expr 1, :idx 2}
+             {:expr 2, :idx 3}
+             {:expr "hello", :idx 1}
+             {:expr :a, :idx 4}
+             {:expr
+              #{{:expr 1, :idx 2}
+                {:expr 2, :idx 3}
+                {:expr "hello", :idx 1}
+                {:expr :a, :idx 4}},
+              :idx 5,
+              :schema #:skeptic.analysis{:placeholders '(2 3 1 4)}}]
+           (clean-callbacks analysed)))
+    (is (= [1 2 "hello" :a #{1 2 :a "hello"}]
+           (unannotate-expr analysed))))
+  (let [analysed (->> '(1 2 :a "hello")
+                      schematize/macroexpand-all
+                      sut/annotate-expr
+                      sut/analyse-coll)]
+    (is (= [{:expr 1, :idx 1}
+             {:expr 2, :idx 2}
+             {:expr :a, :idx 3}
+             {:expr "hello", :idx 4}
+             {:expr
+              [{:expr 1, :idx 1}
+               {:expr 2, :idx 2}
+               {:expr :a, :idx 3}
+               {:expr "hello", :idx 4}],
+              :idx 5,
+              :schema #:skeptic.analysis{:placeholders '(1 2 3 4)}}]
+           (clean-callbacks analysed)))
+    (is (= [1 2 :a "hello" '(1 2 :a "hello")]
+           (unannotate-expr analysed))))
+  (let [analysed (->> '{:a 1 :b 2 :c 3}
+                      schematize/macroexpand-all
+                      sut/annotate-expr
+                      sut/analyse-coll)]
+    (is (= [{:expr :a, :idx 1}
+            {:expr 1, :idx 2}
+            {:expr :b, :idx 4}
+            {:expr 2, :idx 5}
+            {:expr :c, :idx 7}
+            {:expr 3, :idx 8}
+            {:expr
+             [{:expr [{:expr :a, :idx 1} {:expr 1, :idx 2}], :idx 3}
+              {:expr [{:expr :b, :idx 4} {:expr 2, :idx 5}], :idx 6}
+              {:expr [{:expr :c, :idx 7} {:expr 3, :idx 8}], :idx 9}],
+             :idx 10,
+             :map? true,
+             :schema
+             #:skeptic.analysis{:key-placeholders [1 4 7,]
+                                :val-placeholders [2 5 8]}}]
+           (clean-callbacks analysed)))
+    (is (= '(:a 1 :b 2 :c 3 ([:a 1] [:b 2] [:c 3]))
+           (unannotate-expr analysed))))
+  (let [analysed (->> '[1 2 [3 4 [5]]]
+                      schematize/macroexpand-all
+                      sut/annotate-expr
+                      sut/analyse-coll)]
+    (is (= '({:expr 1, :idx 1}
+           {:expr 2, :idx 2}
+           {:expr
+            [{:expr 3, :idx 3}
+             {:expr 4, :idx 4}
+             {:expr [{:expr 5, :idx 5}], :idx 6}],
+            :idx 7}
+           {:expr
+            [{:expr 1, :idx 1}
+             {:expr 2, :idx 2}
+             {:expr
+              [{:expr 3, :idx 3}
+               {:expr 4, :idx 4}
+               {:expr [{:expr 5, :idx 5}], :idx 6}],
+              :idx 7}],
+            :idx 8,
+            :schema #:skeptic.analysis{:placeholders (1 2 7)}})
+           (clean-callbacks analysed)))
+    (is (= '(1 2 [3 4 [5]] [1 2 [3 4 [5]]])
+           (unannotate-expr analysed))))
+  (let [analysed (->> '{:a 1 :b [:z "hello" #{1 2}] :c {:d 7 :e {:f 9}}}
+                      schematize/macroexpand-all
+                      sut/annotate-expr
+                      sut/analyse-coll)]
+    (is (= '({:expr :a, :idx 1}
+             {:expr 1, :idx 2}
+             {:expr :b, :idx 4}
+             {:expr
+              [{:expr :z, :idx 5}
+               {:expr "hello", :idx 6}
+               {:expr #{{:expr 2, :idx 8} {:expr 1, :idx 7}}, :idx 9}],
+              :idx 10}
+             {:expr :c, :idx 12}
+             {:expr
+              ({:expr [{:expr :d, :idx 13} {:expr 7, :idx 14}], :idx 15}
+               {:expr
+                [{:expr :e, :idx 16}
+                 {:expr
+                  ({:expr [{:expr :f, :idx 17} {:expr 9, :idx 18}], :idx 19}),
+                  :idx 20,
+                  :map? true}],
+                :idx 21}),
+              :idx 22,
+              :map? true}
+             {:expr
+              ({:expr [{:expr :a, :idx 1} {:expr 1, :idx 2}], :idx 3}
+               {:expr
+                [{:expr :b, :idx 4}
+                 {:expr
+                  [{:expr :z, :idx 5}
+                   {:expr "hello", :idx 6}
+                   {:expr #{{:expr 2, :idx 8} {:expr 1, :idx 7}}, :idx 9}],
+                  :idx 10}],
+                :idx 11}
+               {:expr
+                [{:expr :c, :idx 12}
+                 {:expr
+                  ({:expr [{:expr :d, :idx 13} {:expr 7, :idx 14}], :idx 15}
+                   {:expr
+                    [{:expr :e, :idx 16}
+                     {:expr
+                      ({:expr [{:expr :f, :idx 17} {:expr 9, :idx 18}], :idx 19}),
+                      :idx 20,
+                      :map? true}],
+                    :idx 21}),
+                  :idx 22,
+                  :map? true}],
+                :idx 23}),
+              :idx 24,
+              :map? true,
+              :schema
+              #:skeptic.analysis{:key-placeholders (1 4 12),
+                                 :val-placeholders (2 10 22)}})
+           (clean-callbacks analysed)))
+    (is (= '(:a
+             1
+             :b
+             [:z "hello" #{1 2}]
+             :c
+             ([:d 7] [:e ([:f 9])])
+             ([:a 1] [:b [:z "hello" #{1 2}]] [:c ([:d 7] [:e ([:f 9])])]))
            (unannotate-expr analysed)))))
