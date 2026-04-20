1449951fd688a9dfc209fb6ad39eeb0b4201c476
Michael A. <nomicflux@gmail.com>
2026-04-20 09:07:57 -0500

    review cleanup

--- skeptic/test/skeptic/checking/pipeline/malli_contracts_integration_test.clj
+++ skeptic/test/skeptic/checking/pipeline/malli_contracts_integration_test.clj
@@ -1,9 +1,5 @@
 (ns skeptic.checking.pipeline.malli-contracts-integration-test
   (:require [clojure.test :refer [deftest is]]
-            [clojure.walk :as walk]
-            [skeptic.analysis.malli-spec.bridge :as sut]
-            [skeptic.analysis.type-ops :as ato]
-            [skeptic.analysis.types :as at]
             [skeptic.checking.pipeline.support :as ps]))
 
 (deftest combined-success-passes
@@ -15,23 +11,3 @@
   (is (ps/single-failure?
        'skeptic.test-examples.malli-contracts/combined-bad
        :bad-keyword)))
-
-(deftest converter-round-trip-combined
-  (let [schema [:=> [:cat [:maybe :int]] [:or :int :string]]
-        result (sut/malli-spec->type schema)
-        expected (at/->FunT
-                  [(at/->FnMethodT
-                    [(at/->MaybeT (at/->GroundT :int 'Int))]
-                    (ato/union-type [(at/->GroundT :int 'Int)
-                                    (at/->GroundT :str 'Str)])
-                    1
-                    false)])]
-    (is (= expected result))))
-
-(deftest no-dyn-in-combined-type
-  (let [schema [:=> [:cat [:maybe :int]] [:or :int :string]]
-        result (sut/malli-spec->type schema)
-        has-dyn? (some (fn [x]
-                         (= at/Dyn x))
-                       (walk/postwalk (fn [x] x) result))]
-    (is (not has-dyn?))))
