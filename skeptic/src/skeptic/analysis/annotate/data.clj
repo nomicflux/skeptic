11c9d64d842220aba9e3624adb2ad7c55c5dcca9
Michael A. <nomicflux@gmail.com>
2026-05-18 18:33:10 -0500

    Phase 3 (trampoline annotate-runner): migrate data.clj helpers to Step shape
    annotate-def, -vector, -set, -map, -new, -with-meta, -throw, -catch, -try,
    -quote all return runner/Step; recursion sites use runner/call
    (:recurse-step ctx) or runner/sequence-children. annotate-try chains
    direct annotate-catch calls through runner/call. data_test driven via
    runner/run with :recurse-step.

--- skeptic/src/skeptic/analysis/annotate/data.clj
+++ skeptic/src/skeptic/analysis/annotate/data.clj
@@ -2,6 +2,7 @@
   (:require [schema.core :as s]
             [skeptic.analysis.annotate.api :as aapi]
             [skeptic.analysis.annotate.coll :as coll]
+            [skeptic.analysis.annotate.runner :as runner]
             [skeptic.analysis.annotate.schema :as aas]
             [skeptic.analysis.calls :as ac]
             [skeptic.analysis.type-ops :as ato]
@@ -9,107 +10,160 @@
             [skeptic.analysis.value :as av]
             [skeptic.provenance :as prov]))
 
-(s/defn annotate-def :- aas/AnnotatedNode
-  [{:keys [locals] :as ctx} :- s/Any node :- aas/AnnotatedNode]
+(s/defn ^:private finish-def :- runner/Step
+  [ctx :- s/Any node :- aas/AnnotatedNode
+   meta-node :- (s/maybe aas/AnnotatedNode)
+   init-node :- (s/maybe aas/AnnotatedNode)]
   (let [prov (prov/with-ctx ctx)
-        meta-node (when-some [meta-node (:meta node)]
-                    ((:recurse ctx) ctx meta-node))
-        init-node (when-some [init-node (:init node)]
-                    ((:recurse ctx)
-                     (assoc ctx :locals locals :name (:name node))
-                     init-node))
         inner (or (:type init-node) (at/Dyn prov))]
-    (cond-> (assoc node :type (at/->VarT prov inner))
-      meta-node (assoc :meta meta-node)
-      init-node (assoc :init init-node))))
+    (runner/done
+     (cond-> (assoc node :type (at/->VarT prov inner))
+       meta-node (assoc :meta meta-node)
+       init-node (assoc :init init-node)))))
 
-(s/defn annotate-vector :- aas/AnnotatedNode
-  [ctx :- s/Any node :- aas/AnnotatedNode]
-  (let [prov (prov/with-ctx ctx)
-        items (mapv #((:recurse ctx) ctx %) (:items node))
-        item-types (mapv #(ato/normalize-type prov (or (:type %) (at/Dyn prov))) items)]
-    (assoc node
-           :items items
-           :type (at/->SeqT (prov/with-refs prov (mapv prov/of item-types))
-                            (at/pattern-from-prefix-tail item-types nil)
-                            :vector))))
-
-(s/defn annotate-set :- aas/AnnotatedNode
-  [ctx :- s/Any node :- aas/AnnotatedNode]
-  (let [prov (prov/with-ctx ctx)
-        items (mapv #((:recurse ctx) ctx %) (:items node))
-        joined (if (seq items)
-                 (av/type-join* prov (map :type items))
-                 (at/Dyn prov))
-        t (ato/normalize-type prov #{joined})]
-    (assoc node :items items :type (assoc t :prov (prov/with-refs (:prov t) [(prov/of joined)])))))
-
-(s/defn annotate-map :- aas/AnnotatedNode
-  [ctx :- s/Any node :- aas/AnnotatedNode]
-  (let [prov (prov/with-ctx ctx)
-        keys (mapv #((:recurse ctx) ctx %) (:keys node))
-        vals (mapv #((:recurse ctx) ctx %) (:vals node))
-        entries (into {}
-                      (map (fn [[key-node val-node]]
-                             [(ac/map-literal-key-type ctx key-node) (:type val-node)]))
-                      (map vector keys vals))
-        refs (into [] (mapcat (fn [[k v]] [(prov/of k) (prov/of v)])) entries)
-        t (ato/normalize-type prov entries)]
-    (assoc node :keys keys :vals vals :type (assoc t :prov (prov/with-refs (:prov t) refs)))))
-
-(s/defn annotate-new :- aas/AnnotatedNode
-  [ctx :- s/Any node :- aas/AnnotatedNode]
-  (let [class-node ((:recurse ctx) ctx (:class node))
-        args (mapv #((:recurse ctx) ctx %) (:args node))
-        prov (prov/with-ctx ctx)
-        type (or (coll/lazy-seq-new-type class-node args)
-                 (some->> (:val class-node) (av/class->type prov))
-                 (aapi/dyn ctx))]
-    (assoc node :class class-node :args args :type type)))
-
-(s/defn annotate-with-meta :- aas/AnnotatedNode
-  [ctx :- s/Any node :- aas/AnnotatedNode]
-  (let [meta-node ((:recurse ctx) ctx (:meta node))
-        expr-node ((:recurse ctx) ctx (:expr node))]
-    (merge node {:meta meta-node :expr expr-node} (ac/node-info expr-node))))
-
-(s/defn annotate-throw :- aas/AnnotatedNode
-  [ctx :- s/Any node :- aas/AnnotatedNode]
-  (let [prov (prov/with-ctx ctx)
-        exception ((:recurse ctx) ctx (:exception node))]
-    (assoc node :exception exception :type (at/BottomType prov))))
-
-(s/defn annotate-catch :- aas/AnnotatedNode
+(s/defn annotate-def :- runner/Step
   [{:keys [locals] :as ctx} :- s/Any node :- aas/AnnotatedNode]
-  (let [prov (prov/with-ctx ctx)
-        class-node ((:recurse ctx) ctx (:class node))
-        caught-type (or (some->> (:val class-node) (av/class->type prov))
-                        (at/Dyn prov))
-        local-node (assoc (:local node) :type caught-type)
-        body ((:recurse ctx)
-              (assoc ctx :locals (assoc locals (:form (:local node)) {:type caught-type}))
-              (:body node))]
-    (assoc node
-           :class class-node
-           :local local-node
-           :body body
-           :type (:type body))))
+  (let [recurse-step (:recurse-step ctx)
+        init-ctx (assoc ctx :locals locals :name (:name node))
+        run-init (fn [meta-node]
+                   (if-some [init (:init node)]
+                     (runner/call recurse-step init-ctx init
+                                  (fn [init-node]
+                                    (finish-def ctx node meta-node init-node)))
+                     (finish-def ctx node meta-node nil)))]
+    (if-some [meta-node (:meta node)]
+      (runner/call recurse-step ctx meta-node run-init)
+      (run-init nil))))
 
-(s/defn annotate-try :- aas/AnnotatedNode
+(s/defn annotate-vector :- runner/Step
   [ctx :- s/Any node :- aas/AnnotatedNode]
+  (runner/sequence-children
+   ctx (:items node)
+   (fn [items]
+     (let [prov (prov/with-ctx ctx)
+           item-types (mapv #(ato/normalize-type prov (or (:type %) (at/Dyn prov))) items)]
+       (runner/done
+        (assoc node
+               :items items
+               :type (at/->SeqT (prov/with-refs prov (mapv prov/of item-types))
+                                (at/pattern-from-prefix-tail item-types nil)
+                                :vector)))))))
+
+(s/defn annotate-set :- runner/Step
+  [ctx :- s/Any node :- aas/AnnotatedNode]
+  (runner/sequence-children
+   ctx (:items node)
+   (fn [items]
+     (let [prov (prov/with-ctx ctx)
+           joined (if (seq items)
+                    (av/type-join* prov (map :type items))
+                    (at/Dyn prov))
+           t (ato/normalize-type prov #{joined})]
+       (runner/done
+        (assoc node :items items :type (assoc t :prov (prov/with-refs (:prov t) [(prov/of joined)]))))))))
+
+(s/defn annotate-map :- runner/Step
+  [ctx :- s/Any node :- aas/AnnotatedNode]
+  (runner/sequence-children
+   ctx (:keys node)
+   (fn [keys]
+     (runner/sequence-children
+      ctx (:vals node)
+      (fn [vals]
+        (let [prov (prov/with-ctx ctx)
+              entries (into {}
+                            (map (fn [[key-node val-node]]
+                                   [(ac/map-literal-key-type ctx key-node) (:type val-node)]))
+                            (map vector keys vals))
+              refs (into [] (mapcat (fn [[k v]] [(prov/of k) (prov/of v)])) entries)
+              t (ato/normalize-type prov entries)]
+          (runner/done
+           (assoc node :keys keys :vals vals :type (assoc t :prov (prov/with-refs (:prov t) refs))))))))))
+
+(s/defn annotate-new :- runner/Step
+  [ctx :- s/Any node :- aas/AnnotatedNode]
+  (runner/call (:recurse-step ctx) ctx (:class node)
+   (fn [class-node]
+     (runner/sequence-children
+      ctx (:args node)
+      (fn [args]
+        (let [prov (prov/with-ctx ctx)
+              type (or (coll/lazy-seq-new-type class-node args)
+                       (some->> (:val class-node) (av/class->type prov))
+                       (aapi/dyn ctx))]
+          (runner/done
+           (assoc node :class class-node :args args :type type))))))))
+
+(s/defn annotate-with-meta :- runner/Step
+  [ctx :- s/Any node :- aas/AnnotatedNode]
+  (let [recurse-step (:recurse-step ctx)]
+    (runner/call recurse-step ctx (:meta node)
+     (fn [meta-node]
+       (runner/call recurse-step ctx (:expr node)
+        (fn [expr-node]
+          (runner/done
+           (merge node {:meta meta-node :expr expr-node} (ac/node-info expr-node)))))))))
+
+(s/defn annotate-throw :- runner/Step
+  [ctx :- s/Any node :- aas/AnnotatedNode]
+  (runner/call (:recurse-step ctx) ctx (:exception node)
+   (fn [exception]
+     (let [prov (prov/with-ctx ctx)]
+       (runner/done
+        (assoc node :exception exception :type (at/BottomType prov)))))))
+
+(s/defn annotate-catch :- runner/Step
+  [{:keys [locals] :as ctx} :- s/Any node :- aas/AnnotatedNode]
+  (let [recurse-step (:recurse-step ctx)
+        prov (prov/with-ctx ctx)]
+    (runner/call recurse-step ctx (:class node)
+     (fn [class-node]
+       (let [caught-type (or (some->> (:val class-node) (av/class->type prov))
+                             (at/Dyn prov))
+             local-node (assoc (:local node) :type caught-type)
+             body-ctx (assoc ctx :locals (assoc locals (:form (:local node)) {:type caught-type}))]
+         (runner/call recurse-step body-ctx (:body node)
+          (fn [body]
+            (runner/done
+             (assoc node
+                    :class class-node
+                    :local local-node
+                    :body body
+                    :type (:type body))))))))))
+
+(s/defn ^:private finish-try :- runner/Step
+  [ctx :- s/Any node :- aas/AnnotatedNode
+   body :- aas/AnnotatedNode
+   catches :- [aas/AnnotatedNode]
+   finally-node :- (s/maybe aas/AnnotatedNode)]
   (let [prov (prov/with-ctx ctx)
-        body ((:recurse ctx) ctx (:body node))
-        catches (mapv #(annotate-catch ctx %) (:catches node))
-        finally-node (when-some [finally-node (:finally node)]
-                       ((:recurse ctx) ctx finally-node))
         type (av/type-join* prov (cons (:type body) (map :type catches)))
         origin (when (empty? catches) (aapi/node-origin body))]
-    (cond-> (assoc node :body body :catches catches :type type)
-      finally-node (assoc :finally finally-node)
-      origin       (assoc :origin origin))))
+    (runner/done
+     (cond-> (assoc node :body body :catches catches :type type)
+       finally-node (assoc :finally finally-node)
+       origin       (assoc :origin origin)))))
 
-(s/defn annotate-quote :- aas/AnnotatedNode
+(s/defn annotate-try :- runner/Step
   [ctx :- s/Any node :- aas/AnnotatedNode]
-  (let [prov (prov/with-ctx ctx)
-        expr ((:recurse ctx) ctx (:expr node))]
-    (assoc node :expr expr :type (av/type-of-value prov (-> node :form second)))))
+  (let [recurse-step (:recurse-step ctx)]
+    (runner/call recurse-step ctx (:body node)
+     (fn [body]
+       (letfn [(walk-catches [acc remaining]
+                 (if (empty? remaining)
+                   (if-some [fin (:finally node)]
+                     (runner/call recurse-step ctx fin
+                                  (fn [finally-node]
+                                    (finish-try ctx node body acc finally-node)))
+                     (finish-try ctx node body acc nil))
+                   (runner/call annotate-catch ctx (first remaining)
+                                (fn [c] (walk-catches (conj acc c) (rest remaining))))))]
+         (walk-catches [] (:catches node)))))))
+
+(s/defn annotate-quote :- runner/Step
+  [ctx :- s/Any node :- aas/AnnotatedNode]
+  (runner/call (:recurse-step ctx) ctx (:expr node)
+   (fn [expr]
+     (let [prov (prov/with-ctx ctx)]
+       (runner/done
+        (assoc node :expr expr :type (av/type-of-value prov (-> node :form second))))))))
--- skeptic/test/skeptic/analysis/annotate/data_test.clj
+++ skeptic/test/skeptic/analysis/annotate/data_test.clj
@@ -1,12 +1,17 @@
 (ns skeptic.analysis.annotate.data-test
   (:require [clojure.test :refer [deftest is testing]]
             [skeptic.analysis.annotate.data :as sut]
+            [skeptic.analysis.annotate.runner :as runner]
             [skeptic.analysis.types :as at]
             [skeptic.provenance :as prov]
             [skeptic.test-helpers :refer [tp]]))
 
-(defn- make-ctx []
-  (prov/set-ctx {} tp))
+(defn- identity-step [_ctx node] (runner/done node))
+
+(defn- ctx-with-step []
+  (assoc (prov/set-ctx {} tp)
+         :recurse (fn [_ n] n)
+         :recurse-step identity-step))
 
 (defn- make-node-with-items [op items]
   {:op op
@@ -21,12 +26,10 @@
 
 (deftest annotate-vector-threads-refs-test
   (testing "vector with two items threads their provs into result's refs"
-    (let [ctx (make-ctx)
-          int-node {:op :const :type (at/->GroundT tp :int 'Int) :form 1}
+    (let [int-node {:op :const :type (at/->GroundT tp :int 'Int) :form 1}
           str-node {:op :const :type (at/->GroundT tp :str 'Str) :form "x"}
           node (make-node-with-items :vector [int-node str-node])
-          ctx-with-recurse (assoc ctx :recurse (fn [_ n] n))
-          result (sut/annotate-vector ctx-with-recurse node)
+          result (runner/run sut/annotate-vector (ctx-with-step) node)
           result-type (:type result)
           refs (:refs (:prov result-type))]
       (is (= 2 (count refs)))
@@ -35,23 +38,19 @@
 
 (deftest annotate-set-threads-refs-test
   (testing "set with one item threads its prov into result's refs"
-    (let [ctx (make-ctx)
-          int-node {:op :const :type (at/->GroundT tp :int 'Int) :form 1}
+    (let [int-node {:op :const :type (at/->GroundT tp :int 'Int) :form 1}
           node (make-node-with-items :set [int-node])
-          ctx-with-recurse (assoc ctx :recurse (fn [_ n] n))
-          result (sut/annotate-set ctx-with-recurse node)
+          result (runner/run sut/annotate-set (ctx-with-step) node)
           result-type (:type result)
           refs (:refs (:prov result-type))]
       (is (= 1 (count refs))))))
 
 (deftest annotate-map-threads-refs-test
   (testing "map with one key/val pair threads both provs into result's refs"
-    (let [ctx (make-ctx)
-          key-node {:op :const :type (at/->GroundT tp :keyword 'Keyword) :form :a}
+    (let [key-node {:op :const :type (at/->GroundT tp :keyword 'Keyword) :form :a}
           val-node {:op :const :type (at/->GroundT tp :int 'Int) :form 1}
           node (make-node-with-keys-vals [key-node] [val-node])
-          ctx-with-recurse (assoc ctx :recurse (fn [_ n] n))
-          result (sut/annotate-map ctx-with-recurse node)
+          result (runner/run sut/annotate-map (ctx-with-step) node)
           result-type (:type result)
           refs (:refs (:prov result-type))]
       (is (= 2 (count refs)))
